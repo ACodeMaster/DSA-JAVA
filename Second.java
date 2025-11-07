881e702035a2d0d476bb90935af4ea3769954a55
ACodeMaster <dhaygudeanand8@gmail.com>
2025-11-07 15:54:35 +0000

    7/11/25

---
+++ Second.java
@@ -0,0 +1,10 @@
+public class Second{
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        int c=10;
+        int average= (a+b+c)/3;
+
+        System.out.println(average);
+    }
+}
