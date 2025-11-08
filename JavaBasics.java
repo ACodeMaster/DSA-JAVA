f32142299a5bf6d5c9066fa8494c24e9d427d154
ACodeMaster <dhaygudeanand8@gmail.com>
2025-11-08 16:38:36 +0000

    JavaBasics

---
+++ JavaBasics.java
@@ -0,0 +1,12 @@
+public class JavaBasics {
+ public static int multiply(int a, int b) {
+    int product = a * b;
+    return product;
+  }
+  public static void main(String[] args) {
+    int a=11;
+    int b=11;  
+    int prod=multiply(a,b);
+        System.out.println("The product is: " + prod);
+  }
+}
