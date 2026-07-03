469531e239e355b30612f2f5104093dd619eb9c6
ACodeMaster <dhaygudeanand8@gmail.com>
2026-07-03 18:08:06 +0000

    Bubble sort example

---
+++ Sorting/BubbleSortExample.java
@@ -0,0 +1,25 @@
+public class BubbleSortExample {
+    public static void main(String[] args) {
+
+        int[] arr = {5, 3, 8, 4, 2};
+        int n = arr.length;
+        // Bubble Sort
+        for (int i=0; i<n-1; i++) {
+            for (int j=0; j<n-1-i; j++) {
+                if (arr[j] > arr[j+1]) {
+
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Sorted Array:");
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
