d7cd668136dabbe71c01d42d30d3a00b014e919b
ACodeMaster <dhaygudeanand8@gmail.com>
2026-07-20 16:22:20 +0000

    First missing positive nuber in array

---
+++ Array/FirstMissingPositive.java
@@ -0,0 +1,27 @@
+public class FirstMissingPositive {
+
+    public static int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] >= 1 &&
+                   nums[i] <= n &&
+                   nums[i] != nums[nums[i] - 1]) {
+                int correctIndex = nums[i] - 1;
+                int temp = nums[i];
+                nums[i] = nums[correctIndex];
+                nums[correctIndex] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {3, 4, -1, 1};
+        int ans = firstMissingPositive(nums);
+        System.out.println("Missing Positive: " + ans);
+    }
+}
